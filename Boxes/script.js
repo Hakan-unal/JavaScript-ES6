2da7d17bdcfe15debd8ee00aeaf8c0f03982b344
Hakan-unal <hakanunal_1996@hotmail.com>
2019-12-27 20:39:19 +0300

    updated code

--- Boxes/script.js
+++ Boxes/script.js
@@ -3,10 +3,29 @@ let value, color;
 color = (event) => {
 
     if (event.target.classList[0] == "box") {
-        console.log("bir");
         switch (event.target.id) {
             case "1": event.target.id = "color";
                 break;
+            case "2": event.target.id = "color";
+                break;
+            case "3": event.target.id = "color";
+                break;
+            case "4": event.target.id = "color";
+                break;
+            case "5": event.target.id = "color";
+                break;
+            case "6": event.target.id = "color";
+                break;
+            case "7": event.target.id = "color";
+                break;
+            case "8": event.target.id = "color";
+                break;
+            case "9": event.target.id = "color";
+                break;
+            case "color": event.target.id = "1";
+                break;
+            default: event.target.id = "box";
+
         }
 
     }
