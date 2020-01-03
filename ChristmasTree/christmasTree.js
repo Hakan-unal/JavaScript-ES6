5dd0b310f2c9d61dc7a2dd869d6fde5e96786fbd
Hakan-unal <hakanunal_1996@hotmail.com>
2020-01-03 22:55:53 +0300

    added comment lines

--- ChristmasTree/christmasTree.js
+++ ChristmasTree/christmasTree.js
@@ -1,25 +1,31 @@
 let sag, sol, fonk1, fonk2, count;
 
-count = 0;
+// html dökümanı üzerinde gerekli elementler yakalandı
 sag = document.getElementsByClassName("r");
 sol = document.getElementsByClassName("l");
 
 fonk1 = () => {
-
+    // sag olarak tanımlanan array'in uzunluğu kadar çalıştı döngü
     for (let i = 0; i < sag.length; i++) {
+        // sag array'inin içerisindeki tüm elemanların id etiketlerine l eklendi
         sag[i].setAttribute("id", "l");
     }
+    // sol olarak tanımlanan array'in uzunluğu kadar çalıştı döngü
     for (let i = 0; i < sol.length; i++) {
+        // sol array'inin içerisindeki tüm elemanların id etiketlerine r eklendi
         sol[i].setAttribute("id", "r");
     }
 }
 
 fonk2 = () => {
-
+    // sag olarak tanımlanan array'in uzunluğu kadar çalıştı döngü
     for (let i = 0; i < sag.length; i++) {
+        // sag array'inin içerisindeki tüm elemanların id etiketlerine r eklendi
         sag[i].setAttribute("id", "r");
     }
+    // sol olarak tanımlanan array'in uzunluğu kadar çalıştı döngü
     for (let i = 0; i < sol.length; i++) {
+        // sol array'inin içerisindeki tüm elemanların id etiketlerine r eklendi
         sol[i].setAttribute("id", "l");
     }
 }
