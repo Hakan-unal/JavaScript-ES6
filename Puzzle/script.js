937c248dd51a7ca67f00269ffc6a15a1c56a7234
Hakan-unal <hakanunal_1996@hotmail.com>
2020-01-26 21:47:10 +0300

    updated

--- Puzzle/script.js
+++ Puzzle/script.js
@@ -3,57 +3,136 @@ let tablo, fonki;
 fonki = (event) => {
     switch (event.target.id) {
         case "b1":
-            document.getElementById("b1").style.background = "white";
-            break;
+            let b1 = document.getElementById("b1").style.background;
+            if (b1 == "white") {
+                document.getElementById("b1").style.background = "black";
+            }
+            else {
+                document.getElementById("b1").style.background = "white";
+            } break;
         case "b2":
-            document.getElementById("b2").style.background = "white";
-            break;
+            let b2 = document.getElementById("b2").style.background;
+            if (b2 == "white") {
+                document.getElementById("b2").style.background = "black";
+            }
+            else {
+                document.getElementById("b2").style.background = "white";
+            } break;
         case "b3":
-            document.getElementById("b3").style.background = "white";
-            break;
+            let b3 = document.getElementById("b3").style.background;
+            if (b3 == "white") {
+                document.getElementById("b3").style.background = "black";
+            }
+            else {
+                document.getElementById("b3").style.background = "white";
+            } break;
         case "b4":
-            document.getElementById("b4").style.background = "white";
-            break;
+            let b4 = document.getElementById("b4").style.background;
+            if (b4 == "white") {
+                document.getElementById("b4").style.background = "black";
+            }
+            else {
+                document.getElementById("b4").style.background = "white";
+            } break;
         case "b5":
-            document.getElementById("b5").style.background = "white";
-            break;
+            let b5 = document.getElementById("b5").style.background;
+            if (b5 == "white") {
+                document.getElementById("b5").style.background = "black";
+            }
+            else {
+                document.getElementById("b5").style.background = "white";
+            } break;
         case "b6":
-            document.getElementById("b6").style.background = "white";
-            break;
+            let b6 = document.getElementById("b6").style.background;
+            if (b6 == "white") {
+                document.getElementById("b6").style.background = "black";
+            }
+            else {
+                document.getElementById("b6").style.background = "white";
+            } break;
         case "b7":
-            document.getElementById("b7").style.background = "white";
-            break;
+            let b7 = document.getElementById("b7").style.background;
+            if (b7 == "white") {
+                document.getElementById("b7").style.background = "black";
+            }
+            else {
+                document.getElementById("b7").style.background = "white";
+            } break;
         case "b8":
-            document.getElementById("b8").style.background = "white";
-            break;
+            let b8 = document.getElementById("b8").style.background;
+            if (b8 == "white") {
+                document.getElementById("b8").style.background = "black";
+            }
+            else {
+                document.getElementById("b8").style.background = "white";
+            } break;
         case "b9":
-            document.getElementById("b9").style.background = "white";
-            break;
+            let b9= document.getElementById("b9").style.background;
+            if (b9 == "white") {
+                document.getElementById("b9").style.background = "black";
+            }
+            else {
+                document.getElementById("b9").style.background = "white";
+            } break;
         case "b10":
-            document.getElementById("b10").style.background = "white";
-            break;
+            let b10 = document.getElementById("b10").style.background;
+            if (b10 == "white") {
+                document.getElementById("b10").style.background = "black";
+            }
+            else {
+                document.getElementById("b10").style.background = "white";
+            } break;
         case "b11":
-            document.getElementById("b11").style.background = "white";
-            break;
+            let b11 = document.getElementById("b11").style.background;
+            if (b11 == "white") {
+                document.getElementById("b11").style.background = "black";
+            }
+            else {
+                document.getElementById("b11").style.background = "white";
+            } break;
         case "b12":
-            document.getElementById("b12").style.background = "white";
-            break;
+            let b12 = document.getElementById("b12").style.background;
+            if (b12 == "white") {
+                document.getElementById("b12").style.background = "black";
+            }
+            else {
+                document.getElementById("b12").style.background = "white";
+            } break;
         case "b13":
-            document.getElementById("b13").style.background = "white";
-            break;
+            let b13 = document.getElementById("b13").style.background;
+            if (b13 == "white") {
+                document.getElementById("b13").style.background = "black";
+            }
+            else {
+                document.getElementById("b13").style.background = "white";
+            } break;
         case "b14":
-            document.getElementById("b14").style.background = "white";
-            break;
+            let b14 = document.getElementById("b14").style.background;
+            if (b14 == "white") {
+                document.getElementById("b14").style.background = "black";
+            }
+            else {
+                document.getElementById("b14").style.background = "white";
+            } break;
         case "b15":
-            document.getElementById("b15").style.background = "white";
-            break;
+            let b15 = document.getElementById("b15").style.background;
+            if (b15 == "white") {
+                document.getElementById("b15").style.background = "black";
+            }
+            else {
+                document.getElementById("b15").style.background = "white";
+            } break;
         case "b16":
-            document.getElementById("b16").style.background = "white";
-            break;
+            let b16 = document.getElementById("b16").style.background;
+            if (b16 == "white") {
+                document.getElementById("b16").style.background = "black";
+            }
+            else {
+                document.getElementById("b16").style.background = "white";
+            } break;
         default: console.log("invalid value");
     }
 }
-
 
 tablo = document.querySelector("#contain");
 tablo.addEventListener("click", (event) => {
