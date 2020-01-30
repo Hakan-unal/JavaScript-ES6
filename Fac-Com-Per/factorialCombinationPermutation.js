b6900f793779d02d1d17efe3451f6cb41437b3c8
Hakan-unal <hakanunal_1996@hotmail.com>
2020-01-30 12:00:04 +0300

    initial

---
+++ Fac-Com-Per/factorialCombinationPermutation.js
@@ -0,0 +1,7 @@
+let input, output, factorialButton, combinationButton, permutationButton;
+
+input = window.document.querySelector("#input");
+output = window.document.querySelector("#output");
+factorialButton = window.document.querySelector("#factorial");
+combinationButton = window.document.querySelector("#combination");
+permutationButton = window.document.querySelector("#permutation");
