bee631b52032574945d12b7a5cabe5d1397dd9c3
Hakan-unal <hakanunal_1996@hotmail.com>
2020-03-04 22:03:46 +0300

    updated

--- SquareMatris/matris.js
+++ SquareMatris/matris.js
@@ -1,20 +1,25 @@
 const container = document.querySelector("#container");
 const input = document.querySelector("#input");
-const button = document.querySelector("#button");
-
+const createMatrisButton = document.querySelector("#createMatrisButton");
 
 const MakeMatrisTable = (val) => {
     for (let i = 0; i < val; i++) {
 
         let row = document.createElement("div");
         row.setAttribute("id", `row${i}`);
-        row.setAttribute("class", "row");
+        row.setAttribute("class", "row my-3");
 
         for (let j = 0; j < val; j++) {
+
+            let input = document.createElement("input");
+            input.setAttribute("class", "input");
+            input.setAttribute("type", "number");
             let col = document.createElement("div");
             col.setAttribute("id", `col${i}${j}`);
-            col.setAttribute("class", "col border");
+            col.setAttribute("class", "col");
+            col.appendChild(input);
             row.appendChild(col);
+
             if (j == val - 1) {
                 container.appendChild(row);
             }
@@ -22,10 +27,9 @@ const MakeMatrisTable = (val) => {
     }
 }
 
-
-
-button.addEventListener("click", () => {
+createMatrisButton.addEventListener("click", () => {
+    container.innerHTML = "";
     let num = parseInt(input.value);
     MakeMatrisTable(num);
-    console.log(container);
+    input.value = "";
 });
