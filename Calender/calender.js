90d962c1e3a728cea7f7fbbfecbe5c506fe5b52f
Hakan-unal <hakanunal_1996@hotmail.com>
2020-03-06 15:06:37 +0300

    updated

--- Calender/calender.js
+++ Calender/calender.js
@@ -1,10 +1,12 @@
 const takvim = document.querySelector("#takvim");
 const info = document.querySelector("#info");
 const newYear = document.querySelector("#newYear");
+const month = document.querySelector("#month");
+
 let day;
 
 let obj = new Date();
-obj.setMonth(1);
+
 
 const createTable = (val) => {
     if (day == 0) {
@@ -16,9 +18,7 @@ const createTable = (val) => {
     }
 }
 
-
-
-const findMonth = (obj) => {
+const findDay = (obj) => {
     for (let i = 1; i <= 32; i++) {
         obj.setDate(i);
         let date = obj.getDate();
@@ -31,4 +31,28 @@ const findMonth = (obj) => {
     }
 }
 
-findMonth(obj);
+
+const findMonth = () => {
+    let value = obj.getMonth();
+    console.log(value);
+    switch (value) {
+        case 0: month.innerText = "January"; break;
+        case 1: month.innerText = "February"; break;
+        case 2: month.innerText = "March"; break;
+        case 3: month.innerText = "April"; break;
+        case 4: month.innerText = "May"; break;
+        case 5: month.innerText = "June"; break;
+        case 6: month.innerText = "July"; break;
+        case 7: month.innerText = "August"; break;
+        case 8: month.innerText = "September"; break;
+        case 9: month.innerText = "October"; break;
+        case 10: month.innerText = "November"; break;
+        case 11: month.innerText = "December"; break;
+        default: month.innerText = "Invalid Month"; break;
+    }
+}
+findMonth();
+
+findDay(obj);
+
+
